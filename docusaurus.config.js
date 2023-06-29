0ba3ca1c23ed17ebe97a0b76b5cd3839885aac97
Matthew Mirman <matt@anarchy.ai>
2023-06-29 18:22:21 +0200

    fixed deployment branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'anarchy-ai', // Usually your GitHub org/user name.
   projectName: 'anarchy-ai.github.io', // Usually your repo name.
-  deploymentBranch: 'main', 
+  deploymentBranch: 'gh-pages', 
 
 
   onBrokenLinks: 'throw',
