d23f0ff57fdc9b6e1251627f72d099ed6cce31b0
Ary Pathania <aryamanp@amazon.com>
2023-07-07 15:17:51 -0700

    using a different url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/AnarchyLogoGithub.png',
 
   // Set the production url of your site here
-  url: 'https://anarchy-ai.github.io',
+  url: 'https://anarchy.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
